670d761ec29335f14e1eb2a6380e4290b2ad4a1a
oliver mueller <omue72@gmx.de>
2025-10-17 16:34:34 +0200

    added pokemon and trainer index and show

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,6 +1,16 @@
 class PokemonsController < ApplicationController
+  before_action :set_pokemon, only: [:show]
 
   def index
     @pokemons = Pokemon.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_pokemon
+    @pokemon = Pokemon.find(params[:id])
   end
 end
